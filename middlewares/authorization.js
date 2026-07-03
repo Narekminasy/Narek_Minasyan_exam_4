a545354d1245b78c6c554ae3f11029cd933651fe
Narek Minasyan <narekminasyan52@gmail.com>
2026-07-03 11:59:57 +0400

    uplods and validation logic

---
+++ middlewares/authorization.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import HttpErrors from "http-errors";
+
+import Users from "../models/Users.js";
+
+const { TOKEN_SECRET } = process.env;
+
+export default async (req, res, next) => {
+    try {
+        const token =
+            String(req.headers?.authorization).replace("Bearer ", "").trim()
+
+        if (!token) {
+            next(new HttpErrors(401));
+            return;
+        }
+
+        let decryptData = null;
+
+        try {
+            decryptData = jwt.verify(token, TOKEN_SECRET);
+        } catch (err) {
+            console.log(err.message);
+        }
+
+        if (!decryptData || !decryptData?.userId) {
+            next(new HttpErrors(401));
+            return;
+        }
+
+        req.userId = decryptData?.userId;
+
+        const user = await Users.findByPk(req.userId);
+
+        if (!user) {
+            next(new HttpErrors(401));
+            return;
+        }
+
+        next();
+    } catch (err) {
+        console.log(err);
+        next(new HttpErrors(401));
+    }
+};
---
+++ middlewares/upload.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "instagram-clone",
+        allowed_formats: ["jpg", "jpeg", "png", "webp", "gif"],
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 20 * 1024 * 1024,
+    },
+    fileFilter: function (req, file, cb) {
+        if (!file.mimetype.startsWith("image/")) {
+            return cb(new Error("Only image files are allowed!"), false);
+        }
+        cb(null, true);
+    },
+});
+
+export default upload;
